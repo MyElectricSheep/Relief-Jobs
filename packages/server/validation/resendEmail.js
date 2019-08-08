87f3208f09534dfeca7a9ebb7530706d4d2e0b40
Electric_Sheep <iamthebou@gmail.com>
2019-08-08 14:17:17 +0200

    Added a utility to check resend email fields when the resend email route is called

---
+++ packages/server/validation/resendEmail.js
@@ -0,0 +1,22 @@
+const validator = require("validator");
+const ifEmpty = require("./checkEmpty");
+
+const checkResendField = data => {
+  let errors = {};
+
+  data.email = !ifEmpty(data.email) ? data.email : "";
+
+  if (validator.isEmpty(data.email)) {
+    errors.email = "Email is required";
+  }
+  if (!validator.isEmail(data.email)) {
+    errors.email = "Invalid Email address";
+  }
+
+  return {
+    errors,
+    isValid: ifEmpty(errors)
+  };
+};
+
+module.exports = checkResendField;
