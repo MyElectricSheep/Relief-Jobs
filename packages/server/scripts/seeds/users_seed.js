e9cb3cdd38d2bd768bd5daf521cc2524f8d0a428
Electric_Sheep <iamthebou@gmail.com>
2019-08-12 11:29:13 +0200

    feat(seed): seed user's passwords is now hashed

--- packages/server/scripts/seeds/users_seed.js
+++ packages/server/scripts/seeds/users_seed.js
@@ -1,19 +1,32 @@
 const Chance = require("chance");
 const chance = new Chance();
+const bcrypt = require("bcryptjs");
 
-const createFakeUser = () => ({
+const saltRounds = 12;
+
+const getHashedPassword = async () => {
+  const hashedPassword = await new Promise((resolve, reject) => {
+    bcrypt.hash(chance.string({ length: 10 }), saltRounds, function(err, hash) {
+      if (err) reject(err);
+      resolve(hash);
+    });
+  });
+  return hashedPassword;
+};
+
+const createFakeUser = async () => ({
   email: chance.email(),
   username: chance.name(),
   phone_number: chance.phone(),
-  password: chance.string({ length: 9 }),
+  password: await getHashedPassword(),
   email_verified: chance.bool()
 });
 
 exports.seed = async function(knex) {
   const fakeUsers = [];
-  const numberOfFakeUsers = 10;
+  const numberOfFakeUsers = 25; // How many users to seed the database with
   for (let i = 0; i < numberOfFakeUsers; i++) {
-    fakeUsers.push(createFakeUser());
+    fakeUsers.push(await createFakeUser());
   }
   // Deletes all existing entries
   await knex("users")
