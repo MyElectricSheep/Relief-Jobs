0b1ce0e5ef6043a447130c7f77d3038c69023d20
Electric_Sheep <iamthebou@gmail.com>
2019-08-13 11:52:04 +0200

    refacto(tests): changed the way the user tests are handled

--- packages/server/tests/test_users.js
+++ packages/server/tests/test_users.js
@@ -6,13 +6,14 @@ const chaiHttp = require("chai-http");
 const server = require("../server");
 const Chance = require("chance");
 const chance = new Chance();
+const database = require("../scripts/knex");
 
 chai.use(chaiHttp);
 
 describe("POST /v1/users/register", () => {
   it("should create a new valid user (with a token and a hashed password)", done => {
     const newUser = {
-      email: chance.email(),
+      email: "test_new_user@test.com",
       username: chance.name(),
       phone_number: chance.phone(),
       password1: "12345678",
@@ -35,29 +36,39 @@ describe("POST /v1/users/register", () => {
         res.body.message.should.equal("Verification email sent successfully");
         describe("POST /verify/:token", () => {
           it("should verify a user with the token sent by email", done => {
-            chai
-              .request(server)
-              .post(`/v1/users/verify/${res.body.data[0].token}`)
-              .send()
-              .end((err, res) => {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.data[0].should.have.property("email_verified");
-                res.body.data[0].email_verified.should.equal(true);
-                res.body.data[0].should.have.property("token_used_before");
-                res.body.data[0].token_used_before.should.equal(true);
-                done();
+            database("users")
+              .select("token")
+              .where({ email: "test_new_user@test.com" })
+              .then(res => {
+                chai
+                  .request(server)
+                  .post(`/v1/users/verify/${res[0].token}`)
+                  .send()
+                  .end((err, res) => {
+                    res.should.have.status(200);
+                    res.should.be.json;
+                    res.body.data[0].should.have.property("email_verified");
+                    res.body.data[0].email_verified.should.equal(true);
+                    res.body.data[0].should.have.property("token_used_before");
+                    res.body.data[0].token_used_before.should.equal(true);
+                    done();
+                  });
               });
           });
           it("should not verify a user who is already verified", done => {
-            chai
-              .request(server)
-              .post(`/v1/users/verify/${res.body.data[0].token}`)
-              .send()
-              .end((err, res) => {
-                res.should.have.status(400);
-                res.should.be.json;
-                done();
+            database("users")
+              .select("token")
+              .where({ email: "test_new_user@test.com" })
+              .then(res => {
+                chai
+                  .request(server)
+                  .post(`/v1/users/verify/${res[0].token}`)
+                  .send()
+                  .end((err, res) => {
+                    res.should.have.status(400);
+                    res.should.be.json;
+                    done();
+                  });
               });
           });
           it("should not verify a user with an expired/wrong token", done => {
@@ -95,3 +106,40 @@ describe("POST /v1/users/register", () => {
       });
   });
 });
+
+describe("POST /v1/users/resend_email", () => {
+  it("should send a new verify token if the email is in the database", done => {
+    const goodEmail = { email: "test_new_user@test.com" };
+    chai
+      .request(server)
+      .post("/v1/users/resend_email")
+      .send(goodEmail)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        done();
+      });
+  });
+  it("should not send a new verify token if the email is wrong / not in the database", done => {
+    const wrongEmail = { email: "test@test.com" };
+    chai
+      .request(server)
+      .post("/v1/users/resend_email")
+      .send(wrongEmail)
+      .end((err, res) => {
+        res.should.have.status(400);
+        res.should.be.json;
+        done();
+      });
+  });
+});
+
+// describe("", () => [
+// it("", done => {
+//   chai
+//     .request(server)
+//     .post("/v1/users/")
+//     .send()
+//     .end((err, res) => {});
+// })
+// ]);
