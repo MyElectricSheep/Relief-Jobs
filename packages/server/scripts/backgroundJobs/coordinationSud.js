1f232a963b2cdb7c47033b44f0fb44005fe783a4
Electric_Sheep <iamthebou@gmail.com>
2019-08-29 18:33:06 +0200

    cleanup(backgroundJobs): cleaned up some comments and console.logs

--- packages/server/scripts/backgroundJobs/coordinationSud.js
+++ packages/server/scripts/backgroundJobs/coordinationSud.js
@@ -3,7 +3,7 @@ const cron = require("node-cron");
 // CRON imports
 const coordinationSudScrapper = require("../../utilities/scrappers/coordinationSudScrapper");
 
-cron.schedule("*/1 * * * *", () => {
+cron.schedule("*/12 * * * *", () => {
   // http://corntab.com/?c=*/12_*_*_*_*
   // Scraps the Coordination Sud jobs database to retrieve latest added jobs every 12 minutes
   coordinationSudScrapper();
