96d19cfe8824b4fa4677feb92103e87b273a6c07
Electric_Sheep <iamthebou@gmail.com>
2019-09-18 13:55:34 +0200

    feat(jobPage): added the capacity to parse the HTML of the job description scrapped from various sources

--- packages/client/src/components/jobPage/JobPageContainer.js
+++ packages/client/src/components/jobPage/JobPageContainer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import parse, { domToReact } from "html-react-parser"; // https://github.com/remarkablemark/html-react-parser
 import PropTypes from "prop-types";
 
 // Material UI imports
@@ -12,7 +13,7 @@ import JobSubtitle from "../jobCard/src/JobSubtitle";
 
 const useStyles = makeStyles(theme => ({
   root: {
-    height: "600px",
+    height: "100%",
     padding: theme.spacing(2, 2)
   }
 }));
@@ -20,18 +21,32 @@ const useStyles = makeStyles(theme => ({
 const JobPageContainer = props => {
   const { jobInfo, fullJobInfo } = props;
   const classes = useStyles();
+
+  const options = {
+    replace: ({ attribs, children }) => {
+      if (!attribs) return;
+
+      if (attribs.href && attribs.href.includes("http")) {
+        return (
+          <a href={attribs.href} style={{ color: "orange", fontWeight: 500 }}>
+            {domToReact(children)}
+          </a>
+        );
+      }
+    }
+  };
+
   if (fullJobInfo) {
     const job = fullJobInfo[0];
     return (
       <Paper className={classes.root}>
         {job.title ? (
-          <Typography variant="h5" component="h3">
+          <Typography variant="h5" component="h4" style={{ fontWeight: 500 }}>
             {job.title}
           </Typography>
         ) : null}
         {jobInfo ? <JobSubtitle jobInfo={jobInfo} /> : null}
-
-        <Typography component="p">This is where the job information will go</Typography>
+        <>{parse(`${job.body_html}`, options)}</>
       </Paper>
     );
   } else return null;
