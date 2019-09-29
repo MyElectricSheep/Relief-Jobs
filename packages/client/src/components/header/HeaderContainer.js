c8219a21000fc2942103a38083b955c4b2d73079
Electric_Sheep <iamthebou@gmail.com>
2019-09-29 15:16:47 +0200

    feat(logo): designed a logo and placed it in the top header

--- packages/client/src/components/header/HeaderContainer.js
+++ packages/client/src/components/header/HeaderContainer.js
@@ -7,6 +7,9 @@ import { withStyles } from "@material-ui/core";
 // Custom components imports
 import HeaderCarousel from "./src/HeaderCarousel";
 
+// Logo
+import reliefJobsLogo from "../../pics/images/reliefJobsLogo.png";
+
 const styles = theme => ({
   carouselPosition: {
     position: "relative"
@@ -18,16 +21,28 @@ const styles = theme => ({
   },
   bringSearchUp: {
     marginBottom: "-2.5em"
+  },
+  logo: {
+    width: "25%",
+    position: "absolute",
+    top: "6.5vh",
+    [theme.breakpoints.down("md")]: {
+      top: "5.9vh"
+    },
+    left: "37.5%",
+    zIndex: 3
   }
 });
 
 const HeaderContainer = props => {
   const {
-    classes: { carouselPosition, test, bringSearchUp }
+    classes: { carouselPosition, test, bringSearchUp, logo }
   } = props;
   return (
     <div className={bringSearchUp}>
+      <img src={reliefJobsLogo} alt="Relief Jobs Logo" className={logo} />
       <HeaderCarousel className={carouselPosition} />
+
       {/* <span className={test}> text positioning test </span> */}
     </div>
   );
