46d0745a773761973f9c59f11981b813cb8072a8
Electric_Sheep <iamthebou@gmail.com>
2019-10-10 11:44:17 +0200

    bugfix(resetButton): fixed a UI bug that made the reset button go to the right side of the screen under certain conditions

--- packages/client/src/components/totalJobs/TotalJobs.js
+++ packages/client/src/components/totalJobs/TotalJobs.js
@@ -24,25 +24,23 @@ const TotalJobs = ({ totalJobs, filters, setFilters, noJobs, setSearchInput, set
   return (
     <>
       {!noJobs && (
-        <Grid item xs={11}>
-          <Typography
-            variant="h2"
-            color="textPrimary"
-            component="h1"
-            style={checkFilters(filters) ? { paddingRight: "0.5em" } : {}}
-            align="center"
-          >
-            {totalJobs > 1 ? (
-              <>
-                {totalJobs} <FormattedMessage id="component.totalJobs.numberOfJobs" />
-              </>
-            ) : (
-              <>
-                {totalJobs} <FormattedMessage id="component.totalJobs.numberOfJobsAlt" />
-              </>
-            )}
-          </Typography>
-        </Grid>
+        <Typography
+          variant="h2"
+          color="textPrimary"
+          component="h1"
+          style={checkFilters(filters) ? { paddingRight: "0.5em" } : {}}
+          align="center"
+        >
+          {totalJobs > 1 ? (
+            <>
+              {totalJobs} <FormattedMessage id="component.totalJobs.numberOfJobs" />
+            </>
+          ) : (
+            <>
+              {totalJobs} <FormattedMessage id="component.totalJobs.numberOfJobsAlt" />
+            </>
+          )}
+        </Typography>
       )}
       {noJobs && (
         <Grid container direction="column" justify="center" alignItems="center">
