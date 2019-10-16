c1c29f1b8665b642163bb0d24dafbc886ed2bb64
Electric_Sheep <iamthebou@gmail.com>
2019-10-16 16:14:19 +0200

    feat(GoogleAnalytics): added Google Analytics tracking to the app

--- packages/client/src/components/routing/MainRouter.js
+++ packages/client/src/components/routing/MainRouter.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 // Routing imports
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, useHistory } from "react-router-dom";
 import JobsRouter from "./JobsRouter";
 
 // Custom components imports
 import NotFound from "../notFound/NotFound";
+
+// Google Analytics import
+import ReactGA from "react-ga";
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_TAG);
 
 const getServerUrl = () => {
   if (process.env.REACT_APP_NODE_ENV === "production") {
@@ -17,22 +21,29 @@ const getServerUrl = () => {
 
 const serverUrl = getServerUrl();
 
-const MainRouter = () => (
-  <>
-    <Switch>
-      {/* GOES TO THE JOBS ROUTER PAGE */}
+const MainRouter = () => {
+  const history = useHistory();
+  history.listen(location => {
+    ReactGA.set({ page: location.pathname }); // Update the user's current page
+    ReactGA.pageview(location.pathname); // Record a pageview for the given page
+  });
+  return (
+    <>
+      <Switch>
+        {/* GOES TO THE JOBS ROUTER PAGE */}
 
-      <Route path="/search" render={props => <JobsRouter {...props} serverUrl={serverUrl} />} />
+        <Route path="/search" render={props => <JobsRouter {...props} serverUrl={serverUrl} />} />
 
-      {/* CATCH ALL ROOT ROUTE >> GOES TO JOBS SEARCH MAIN PAGE */}
+        {/* CATCH ALL ROOT ROUTE >> GOES TO JOBS SEARCH MAIN PAGE */}
 
-      <Route exact path="/" render={props => <JobsRouter {...props} serverUrl={serverUrl} />} />
+        <Route exact path="/" render={props => <JobsRouter {...props} serverUrl={serverUrl} />} />
 
-      {/* 404 NOT FOUND ROUTE */}
+        {/* 404 NOT FOUND ROUTE */}
 
-      <Route component={NotFound} />
-    </Switch>
-  </>
-);
+        <Route component={NotFound} />
+      </Switch>
+    </>
+  );
+};
 
 export default MainRouter;
