98837a34e3256f67e6872999e164e4d951602fdd
Electric_Sheep <iamthebou@gmail.com>
2019-10-17 16:31:49 +0200

    feat(moreFilters): finished boostraping the more filters sub menu component. Expansion panels can now accomodate different kind of filters for further developments

--- packages/client/src/components/searchAndFilter/src/MoreSubMenu.js
+++ packages/client/src/components/searchAndFilter/src/MoreSubMenu.js
@@ -2,8 +2,19 @@ import React from "react";
 import PropTypes from "prop-types";
 
 // Material UI imports
-import { makeStyles } from "@material-ui/core/styles";
-import { FormControl, FormGroup, FormControlLabel, Checkbox } from "@material-ui/core";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import {
+  FormControl,
+  FormGroup,
+  FormControlLabel,
+  Checkbox,
+  ExpansionPanel,
+  ExpansionPanelDetails,
+  Typography,
+  Grid
+} from "@material-ui/core";
+import MuiExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 // i18n imports
 import { FormattedMessage } from "react-intl";
@@ -12,20 +23,49 @@ const useStyles = makeStyles(theme => ({
   formControl: {
     margin: theme.spacing(3),
     maxHeight: "250px",
+    width: "300px",
     overflow: "auto"
   },
   root: {
     color: "#000000"
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.primary
   }
 }));
 
+const ExpansionPanelSummary = withStyles({
+  root: {
+    backgroundColor: "rgba(0, 0, 0, .03)",
+    borderRadius: "5%",
+    overflow: "hidden",
+    marginBottom: -1,
+    minHeight: 56,
+    "&$expanded": {
+      minHeight: 56
+    }
+  },
+  content: {
+    "&$expanded": {
+      margin: "12px 0"
+    }
+  },
+  expanded: {}
+})(MuiExpansionPanelSummary);
+
 const MoreSubMenu = ({ filters, setFilters }) => {
   const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
 
   const handleChange = value => event => {
     setFilters(rest => {
       return { ...rest, language: { ...filters.language, [value]: event.target.checked } };
     });
+  };
+
+  const handleExpansionPanel = panel => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
   };
 
   const {
@@ -58,24 +98,46 @@ const MoreSubMenu = ({ filters, setFilters }) => {
   return (
     <FormControl component="fieldset" className={classes.formControl}>
       <FormGroup>
-        {languageCheckboxes.map(checkbox => {
-          return (
-            <FormControlLabel
-              key={checkbox.term}
-              control={
-                <Checkbox
-                  checked={checkbox.checked}
-                  onChange={handleChange(checkbox.term)}
-                  value={checkbox.term}
-                  color="primary"
-                  className={classes.root}
-                  style={{ marginLeft: "0.5em" }}
-                />
-              }
-              label={checkbox.label}
-            />
-          );
-        })}
+        <ExpansionPanel expanded={expanded === "panel1"} onChange={handleExpansionPanel("panel1")}>
+          <ExpansionPanelSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel1bh-content"
+            id="panel1bh-header"
+          >
+            <Typography variant="body1" color="textPrimary">
+              <FormattedMessage id="component.filtersLanguage.languages" />
+            </Typography>
+            {/* <Typography className={classes.secondaryHeading}>Choose language</Typography> */}
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Grid
+              container
+              direction="column"
+              justify="flex-start"
+              alignItems="flex-start"
+              //   style={{ paddingLeft: "1.1em" }}
+            >
+              {languageCheckboxes.map(checkbox => {
+                return (
+                  <FormControlLabel
+                    key={checkbox.term}
+                    control={
+                      <Checkbox
+                        checked={checkbox.checked}
+                        onChange={handleChange(checkbox.term)}
+                        value={checkbox.term}
+                        color="primary"
+                        className={classes.root}
+                        style={{ marginLeft: "0.5em" }}
+                      />
+                    }
+                    label={checkbox.label}
+                  />
+                );
+              })}
+            </Grid>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </FormGroup>
     </FormControl>
   );
